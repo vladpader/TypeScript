0650632f2c1622979d94692894447c6dcfd12b9a
vladslav312 <150502838+vladpader@users.noreply.github.com>
2024-01-17 18:13:42 +1000

    Add files via upload

---
+++ ооп на TypeScript/oop.ts
@@ -0,0 +1,24 @@
+// // Стрелочная функция, выводящая элементы массива поочередно в консоль
+// const massiv = (num) => {
+//     for (let i = 0; i < num.length; i++) {
+//       console.log(num[i]);
+//     }
+//   }
+  
+//   // Пример использования функции с массивом длиной 5
+//   const myArray = [1, 2, 3, 4, 5];
+//   massiv(myArray);
+// const arr = [1, 2, 3, 4, 5];
+
+// const printArray = (arr) => {
+  // for (let i = 0; i < arr.length; i++) {
+  //   console.log(arr[i]);
+//   }
+// };
+
+// printArray(arr);
+
+const num:number= 5;
+const text:string= 'hello';
+const massiv:number[]=[1,2,3,4];
+const grr: () => void =function rer(){};
